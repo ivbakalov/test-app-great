93871eadc489d66bb8ebe2629897047793e3a2d0
ivaylobakalov <ivailo.bakalov@betengines.com>
2023-03-02 01:32:51 +0200

    remove not found

--- server/server.ts
+++ server/server.ts
@@ -1,7 +1,6 @@
 import * as http from 'http';
 
 import { router } from './router';
-
 import { gallery } from './routes/gallery.route';
 import { posts } from './routes/posts.route';
 
@@ -9,9 +8,8 @@ const hostname = '127.0.0.1';
 const port = process.env.PORT || 3000;
 
 const server = http.createServer((req: http.IncomingMessage, res: http.ServerResponse) => {
-  router(req, res).get('/api/posts', posts);
   router(req, res).get('/api/gallery', gallery);
-  // router(req, res).get('*', notFound);
+  router(req, res).get('/api/posts', posts);
 });
 
 server.on('error', (e) => {
